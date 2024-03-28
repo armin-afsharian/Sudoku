c40ea287ad5b2a4a5fa1309d59858de584ec58ed
armin-afsharian <armin8545@gmail.com>
2024-03-28 17:24:14 -0400

    Fix a problem

--- sudokuSolverAlgo.py
+++ sudokuSolverAlgo.py
@@ -4,7 +4,7 @@ from sudokuGenerator import *
 
 # -------- Global board ----------------
 
-Board = [
+Board1 = [
         [0, 1, 0, 0, 4, 5, 0, 0, 0],
         [6, 9, 0, 0, 0, 0, 0, 0, 0],
         [8, 0, 0, 3, 6, 0, 0, 0, 1],
@@ -16,7 +16,33 @@ Board = [
         [0, 0, 7, 5, 1, 0, 0, 9, 8],
     ]
 
-solvedBoard = copy.deepcopy(Board)
+Board2 = [
+        [0, 9, 3, 0, 0, 0, 0, 6, 4],
+        [0, 0, 0, 4, 6, 3, 5, 0, 8],
+        [0, 4, 8, 9, 2, 0, 0, 0, 0],
+        [8, 6, 4, 5, 3, 7, 9, 0, 1],
+        [0, 0, 7, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 6],
+        [0, 0, 1, 0, 5, 2, 6, 0, 7],
+        [0, 0, 0, 7, 9, 6, 8, 1, 0],
+        [2, 7, 0, 0, 8, 0, 0, 0, 9],
+    ]
+
+TestBoard =[
+                    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+                    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+                    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+                    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+                    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+                    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+                    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+                    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+                    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+                ]
+
+
+Board = []
+solvedBoard = []
 
 def solve(board):
     # end condition:- getting to the end of the board - the function findEmpty return NONE
@@ -38,9 +64,14 @@ def solve(board):
     return False
 
 
-def mainSolver(level):
+def mainSolver(level, boardSelection):
     # sudokuGenerate(Board, level)
+    Board = copy.deepcopy(Board1)
+    if boardSelection == 1:
+        Board = copy.deepcopy(Board2)
+    else:
+        Board = copy.deepcopy(TestBoard)
     solvedBoard = copy.deepcopy(Board)
     solve(solvedBoard)
-    return solvedBoard
+    return solvedBoard, Board
    
